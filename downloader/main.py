3d8c95a13624a3ccf79f3159ecf9b51d052275aa
NicklasMCHD <NicklasMCHD@live.dk>
2017-09-17 14:34:40 +0200

    Added a check for empty folder selection

--- downloader/main.py
+++ downloader/main.py
@@ -20,6 +20,11 @@ app = wx.App()
 app.MainLoop()# Call Dialog
 wx.MessageBox("Welcome.\nThis small program will help you download and install the latest version of Access Stick on an usb.\nBefore continuing, please insert your usb into the computer.\nWhen you've done that, press \"OK\".\n\n\nFor more information visit: https://accessiware.com/accessstick.", "Access Stick Downloader", style=wx.OK)
 usb = GetUSB()
+if usb == "":
+	dlg = wx.MessageDialog(None, "No folder selected. The Access Stick Downloader will now exit.", "Access Stick Downloader", wx.OK | wx.ICON_INFORMATION)
+	dlg.ShowModal()
+	dlg.Destroy()
+	sys.exit()
 while True:
 	q = YesNo(None, "Is \""+usb+"\" the correct usb?")
 	if q == True:
